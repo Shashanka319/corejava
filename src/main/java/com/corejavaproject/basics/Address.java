1473ea139217c471956e840d555667846a546742
Shashank K <sharathshashank1986@gmail.com>
2026-06-01 22:31:41 +0530

    updated files

--- src/main/java/com/corejavaproject/basics/Address.java
+++ src/main/java/com/corejavaproject/basics/Address.java
@@ -6,13 +6,17 @@ import lombok.extern.log4j.Log4j2;
 public class Address {
     String name;
     String address;
-    Address(String name,String address){
+    Mobile mobile;
+    Address(String name,String address,Mobile mobile){
         this.name=name;
         this.address=address;
+        this.mobile=mobile;
     }
     public void info(){
         log.info("Name:"+this.name);
         log.info("Address:"+ this.address);
+        Mobile mobile1 = new Mobile;
+        mobile1.mobileInfo();
     }
 
 
---
+++ src/main/java/com/corejavaproject/basics/Mobile.java
@@ -0,0 +1,7 @@
+package com.corejavaproject.basics;
+
+public class Mobile {
+    public void mobileInfo(){
+        log.info("Mobile Information of the employee");
+    }
+}
