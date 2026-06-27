d6cfb225e48bea6648fc0de4916540e3c8e055c4
Shashank K <sharathshashank1986@gmail.com>
2026-06-27 16:43:33 +0530

    Student Constructor

--- src/main/java/com/corejavaproject/basics/constructor/Student.java
+++ src/main/java/com/corejavaproject/basics/constructor/Student.java
@@ -4,4 +4,18 @@ public class Student {
     private static String INSTITUTE_NAME="Emexo Technology";
     private String name;
     private int age;
+
+    public Student(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    public void getStudent(){
+        System.out.println("Student Name : "+this.name);
+        System.out.println("Student Age : "+this.age);
+        System.out.println("Institute Name : "+INSTITUTE_NAME);
+    }
+    public static void main(String[] args){
+        Student student=new Student("Shashank",25);
+        student.getStudent();
+    }
 }
