5d3d223fde8db276c05c7f293082efb9b8b09ae7
Shashank K <sharathshashank1986@gmail.com>
2026-06-27 16:58:01 +0530

    Customer Constructor

---
+++ src/main/java/com/corejavaproject/basics/constructor/CustomerRunner.java
@@ -0,0 +1,9 @@
+package com.corejavaproject.basics.constructor;
+
+public class CustomerRunner {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Shashanka", 22, 974387398, "Veerabadhra Nilaya Hulimangala", "T-shirt", 500.0);
+        customer.getDetails();
+    }
+
+}
--- src/main/java/com/corejavaproject/basics/constructor/Student.java
+++ src/main/java/com/corejavaproject/basics/constructor/Student.java
@@ -5,6 +5,11 @@ public class Student {
     private String name;
     private int age;
 
+    public Student(){
+        this.name="unkown";
+        this.age=0;
+    }
+
     public Student(String name,int age){
         this.name=name;
         this.age=age;
