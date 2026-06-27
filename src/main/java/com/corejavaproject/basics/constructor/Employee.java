4edf4421010a07234974a7829bfbfc7825e18d28
Shashank K <sharathshashank1986@gmail.com>
2026-06-27 16:31:07 +0530

    Constructor

---
+++ src/main/java/com/corejavaproject/basics/constructor/Employee.java
@@ -0,0 +1,25 @@
+package com.corejavaproject.basics.constructor;
+
+public class Employee {
+    public String COMPANY_NAME="Emexo Technology";
+    private String name;
+    private int age;
+
+    public Employee(){
+        this.name="unkonown";
+        this.age=0;
+    }
+    public Employee(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public void getEmployee(){
+        System.out.println("Employee Name : "+this.name);
+        System.out.println("Employee Age : "+this.age);
+    }
+    public static void main(String[] age){
+        Employee employee = new Employee("Shashanka",22);
+        employee.getEmployee();
+    }
+
+}
