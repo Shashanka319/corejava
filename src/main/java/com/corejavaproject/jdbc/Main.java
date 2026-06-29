5e17fed447df0c29682a5dff4d36e257dbd7c2fc
Shashank K <sharathshashank1986@gmail.com>
2026-06-29 23:18:04 +0530

    updated jdbc

--- src/main/java/com/corejavaproject/jdbc/Main.java
+++ src/main/java/com/corejavaproject/jdbc/Main.java
@@ -10,17 +10,10 @@ public class Main {
 
         User user = new User("Shashank", "Kumar", "natalia@gmail.com", LocalDate.of(2026, 9, 27));
 
-        User  user1 =new User("Sharath","Kumbar","email", LocalDate.of(2026, 9, 27));
-
         UserDAO userDAO = new UserDAOImpl();
         int result = userDAO.save(user);
 
-
-        UserDAO userDAO1 = new UserDAOImpl();
-        int result1 = userDAO1.save(user1);
-
         log.info("Result of saving user: {}", result);
-        log.info("Result of saving user1: {}", result1);
 
 
         log.info("Current users in database: {}", userDAO.getAllUsers());
