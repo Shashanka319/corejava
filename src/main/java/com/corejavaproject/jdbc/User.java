a731e7aca35c7b118e8c43ef648961fce638309f
Shashank K <sharathshashank1986@gmail.com>
2026-06-29 23:15:18 +0530

    JDBC Connection

--- src/main/java/com/corejavaproject/jdbc/User.java
+++ src/main/java/com/corejavaproject/jdbc/User.java
@@ -7,3 +7,4 @@ import java.time.LocalDate;
 public record User(String firstName, String lastName, String email, LocalDate hireDate) {
 
 }
+
--- src/main/java/com/corejavaproject/jdbc/UserDAO.java
+++ src/main/java/com/corejavaproject/jdbc/UserDAO.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface UserDAO {
     int save(User user);
     List<User> getAllUsers();
+
 }
