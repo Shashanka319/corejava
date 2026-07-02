092882eb3f8d0e69eda7cbf9e6502a69e7fd709f
Shashank K <sharathshashank1986@gmail.com>
2026-07-02 06:07:10 +0530

    Exception Hnadling

--- src/main/java/com/corejavaproject/exceptionHanling/InvalidInput.java
+++ src/main/java/com/corejavaproject/exceptionHanling/InvalidInput.java
@@ -1,8 +1,12 @@
 package com.corejavaproject.exceptionHanling;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class InvalidInput extends Exception{
     public InvalidInput(){
         super();
+        log.info("Execute the invalid Input....from Super Class");
     }
 
     public InvalidInput(String message){
