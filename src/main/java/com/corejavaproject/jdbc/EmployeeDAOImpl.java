c41452f6e2f33c519a5dac2ac30ea32be745ad0a
Shashank K <sharathshashank1986@gmail.com>
2026-07-03 22:09:54 +0530

    updated files

--- src/main/java/com/corejavaproject/jdbc/EmployeeDAOImpl.java
+++ src/main/java/com/corejavaproject/jdbc/EmployeeDAOImpl.java
@@ -12,19 +12,19 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public int saveEmployee(Employee employee) {
         int response=0;
-        String sql = "insert into employees(first_name, last_name, email, hire_date) values (?, ?, ?, ?)";
-        try(Connection con=DBConnection.getEmployeeConnection();
-            PreparedStatement statement=con.prepareStatement(sql)){
-                statement.setString(1,employee.firstName());
-                statement.setString(2,employee.lastName());
-                statement.setString(3,employee.email());
-                statement.setObject(4,employee.hireDate());
-                response= statement.executeUpdate();
-        }catch(Exception e){
-            log.error(e);
-        }
+        String sql ="insert into employees(first_name,last_name,email,hire_date) values(?,?,?,?)";
+        try(Connection connection= DBConnection.getEmployeeConnection();
+            PreparedStatement statement=connection.prepareStatement(sql)){
+            statement.setString(1,employee.firstName());
+            statement.setString(2,employee.lastName());
+            statement.setString(3,employee.email());
+            statement.setObject(4,employee.hireDate());
+            response=statement.executeUpdate();
 
-        log.info("Inserted the user details, no of rows inserted:{}", response);
+        }catch (Exception exp){
+            log.error("Exception while saving employee detailes",exp);
+        }
+        log.info("Inserted the user Detailes,no of rows inserted:{}",response);
         return response;
     }
 
