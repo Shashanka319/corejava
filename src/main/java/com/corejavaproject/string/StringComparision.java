d1aac851111887d83c4d2b17a90ecd624ff02134
Shashank K <sharathshashank1986@gmail.com>
2026-07-05 11:12:51 +0530

    String ==

---
+++ src/main/java/com/corejavaproject/string/StringComparision.java
@@ -0,0 +1,29 @@
+package com.corejavaproject.string;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+
+public class StringComparision {
+    public void stringCompare(String str1,String str2){
+        if(str1 == str2){
+            log.info("String Are Equal");
+        }else{
+            log.info("String Are Not Equal");
+        }
+    }
+    public void stringCompare1(String str1,String str2){
+        if(str1 .equals(str2)){
+            log.info("String Are Equal");
+        }else {
+            log.info("String Are Not Equal");
+        }
+    }
+    public void stringCompare2(String str1,String str2){
+        if(str1.compareTo(str2) == 0){
+            log.info("String Are Equal");
+        }else  {
+            log.info("String Are Not Equal");
+        }
+    }
+}
---
+++ src/main/java/com/corejavaproject/string/StringRunner.java
@@ -0,0 +1,10 @@
+package com.corejavaproject.string;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String str="Shashank";
+        String str1 = new String("Shashank");
+        StringComparision stringComparision=new StringComparision();
+        stringComparision.stringCompare(str,str1);
+    }
+}
