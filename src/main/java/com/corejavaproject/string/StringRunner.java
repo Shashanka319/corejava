2bd137f0de01094c55000944c77dcdb5c9235085
Shashank K <sharathshashank1986@gmail.com>
2026-07-05 11:21:22 +0530

    invoke the equal Ignorance

--- src/main/java/com/corejavaproject/string/StringRunner.java
+++ src/main/java/com/corejavaproject/string/StringRunner.java
@@ -3,10 +3,11 @@ package com.corejavaproject.string;
 public class StringRunner {
     public static void main(String[] args) {
         String str="Shashank";
-        String str1 = new String("Shashank");
+        String str1 = new String("Shashank K");
         StringComparision stringComparision=new StringComparision();
         stringComparision.stringCompare(str,str1);
         stringComparision.stringCompare1(str,str1);
         stringComparision.stringCompare2(str,str1);
+        stringComparision.eualIgnore(str,str1);
     }
 }
