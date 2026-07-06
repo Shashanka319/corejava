2dc04718e2ab30249fdaa7006c542c0e8a5dcd15
Shashank K <sharathshashank1986@gmail.com>
2026-07-06 23:51:04 +0530

    hello world

---
+++ src/main/java/com/corejavaproject/basics/constructor/Worker.java
@@ -0,0 +1,8 @@
+package com.corejavaproject.basics.constructor;
+
+public class Worker {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
