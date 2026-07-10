5d0dd561971c40ff7d8e81f9e2a2f8d91d27edac
Shashank K <sharathshashank1986@gmail.com>
2026-07-10 20:56:52 +0530

    updated

--- src/main/java/com/corejavaproject/string/StringBuilderRunner.java
+++ src/main/java/com/corejavaproject/string/StringBuilderRunner.java
@@ -2,10 +2,7 @@ package com.corejavaproject.string;
 
 public class StringBuilderRunner {
         public static void main(String[] args) {
-
             StringBuilder stringBuilderExample = new StringBuilder();
             stringBuilderExample.display("hi","hello","Shashank");
         }
-
-
 }
