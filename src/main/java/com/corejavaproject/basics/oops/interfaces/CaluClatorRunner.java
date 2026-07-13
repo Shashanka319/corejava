b29dd4a3d55ad87dc059c47972ef184b68b9454f
Shashank K <sharathshashank1986@gmail.com>
2026-07-13 19:14:34 +0530

    Result

---
+++ src/main/java/com/corejavaproject/basics/oops/interfaces/CaluClatorRunner.java
@@ -0,0 +1,15 @@
+package com.corejavaproject.basics.oops.interfaces;
+
+public class CaluClatorRunner {
+    public static void main(String[] args) {
+        Caluclator1 caluclator1 = new Caluclator1();
+        CaluClator2 caluClator2 = new CaluClator2();
+        Caluclator3 caluclator3 = new Caluclator3();
+        caluclator1.sum();
+        caluclator1.sub();
+        caluClator2.sum();
+        caluClator2.sub();
+        caluclator3.sum();
+        caluclator3.sub();
+    }
+}
