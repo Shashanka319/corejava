bb1549058a5f55ba1d692282ba23b6dccb21c629
Shashank K <sharathshashank1986@gmail.com>
2026-07-22 08:31:06 +0530

    1D Array of one Approch

--- src/main/java/com/corejavaproject/arrays/Array.java
+++ src/main/java/com/corejavaproject/arrays/Array.java
@@ -13,42 +13,6 @@ public class Array {
         }
         System.out.println();
     }
-    public  void getApproch2OfOne() {
-        System.out.println("Execute the one dimential array using Another Approch ");
-        int[] array = {10, 20, 30};
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("array[" + i + "] = " + array[i]);
-        }
-        System.out.println();
-    }
+    
 
-    public  void getMutliApproch1() {
-        System.out.println("Execute the multidimensional array using one  Approach");
-
-        int[][] array = new int[2][2];
-        array[0][0] = 1;
-        array[0][1] = 2;
-        array[1][0] = 1;
-        array[1][1] = 2;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-        }
-        System.out.println();
-    }
-    public  void getMutliApproch2() {
-        System.out.println("Execute the multidimensional array using one  Approach");
-
-        int[][] array = {
-                {1, 2},
-                {1, 2}};
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
-            }
-        }
-    }
 }
