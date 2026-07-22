8d031183dbac98a5032f8d4fe7c45c397ce6e1fd
Shashank K <sharathshashank1986@gmail.com>
2026-07-22 08:33:30 +0530

    2D Array of one Approch

--- src/main/java/com/corejavaproject/arrays/Array.java
+++ src/main/java/com/corejavaproject/arrays/Array.java
@@ -22,6 +22,21 @@ public class Array {
         System.out.println();
     }
 
+    public  void getMutliApproch1() {
+        System.out.println("Execute the multidimensional array using one  Approach");
 
-   
+        int[][] array = new int[2][2];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[1][0] = 1;
+        array[1][1] = 2;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+        }
+        System.out.println();
+    }
+
 }
