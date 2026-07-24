3665207141b48b7bb4f8b1f64f4c09c5fdd15d3e
Shashank K <sharathshashank1986@gmail.com>
2026-07-24 22:25:06 +0530

    Runnable method

--- src/main/java/com/corejavaproject/mutlithreadingpractice/MainBlock.java
+++ src/main/java/com/corejavaproject/mutlithreadingpractice/MainBlock.java
@@ -9,16 +9,16 @@ public class MainBlock {
         t.start();
 
         SynchronizationBlock sb1 = new SynchronizationBlock();
-        sb.objLevelLock();
-        sb.classLevelLock();
-        Thread t = new Thread(sb);
-        t.start();
+        sb1.objLevelLock();
+        sb1.classLevelLock();
+        Thread t1 = new Thread(sb1);
+        t1.start();
 
-        SynchronizationBlock sb = new SynchronizationBlock();
-        sb.objLevelLock();
-        sb.classLevelLock();
-        Thread t = new Thread(sb);
-        t.start();
+        SynchronizationBlock sb2 = new SynchronizationBlock();
+        sb2.objLevelLock();
+        sb2.classLevelLock();
+        Thread t2 = new Thread(sb2);
+        t2.start();
 
 
     }
