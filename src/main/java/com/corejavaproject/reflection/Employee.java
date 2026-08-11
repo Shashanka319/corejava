2210d9c3bb673f966850be24f33a61c660dbb7db
Shashank K <sharathshashank1986@gmail.com>
2026-08-11 09:54:25 +0530

    Reflection

---
+++ src/main/java/com/corejavaproject/reflection/Employee.java
@@ -0,0 +1,29 @@
+package com.corejavaproject.reflection;
+
+public class Employee {
+    private int id;
+
+    private static String NAME;
+
+    public String add;
+
+    public static long MOBILE_NO;
+
+    private static double getsalary(){
+        return getsalary();
+
+    }
+
+    private double getSalary(double salary){
+        return salary;
+    }
+
+    public static String getMail(){
+        return getMail();
+    }
+
+    public String getMail(String mail){
+        return mail;
+    }
+
+}
