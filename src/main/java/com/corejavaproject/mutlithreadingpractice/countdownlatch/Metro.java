fb645217a5086c48e0f2f37dbd174129b82d1691
Shashank K <sharathshashank1986@gmail.com>
2026-09-01 09:03:52 +0530

    Metro

---
+++ src/main/java/com/corejavaproject/mutlithreadingpractice/countdownlatch/Metro.java
@@ -0,0 +1,22 @@
+package com.corejavaproject.mutlithreadingpractice.countdownlatch;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.concurrent.CountDownLatch;
+@Log4j2
+public class Metro {
+    CountDownLatch latch;
+    Metro(CountDownLatch latch){
+        this.latch = new CountDownLatch(1);
+    }
+    public void getLatch(){
+        System.out.println("Taking ticket from counter");
+        try{
+            Thread.sleep(8000);
+        }catch (InterruptedException e){
+            log.error("Issues occur while ticket taking",e);
+        }
+        log.info("Ticket confirmed");
+        latch.countDown();
+    }
+}
---
+++ src/main/java/com/corejavaproject/mutlithreadingpractice/countdownlatch/MetroRunner.java
@@ -0,0 +1,34 @@
+package com.corejavaproject.mutlithreadingpractice.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class MetroRunner {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(4);
+
+        Metro metro = new Metro(latch);
+        Metro metro2 = new Metro(latch);
+        Metro metro3 = new Metro(latch);
+        Metro metro4 = new Metro(latch);
+
+        Runnable r1 =metro::getLatch;
+        Runnable r2 =metro2::getLatch;
+        Runnable r3 =metro3::getLatch;
+        Runnable r4 =metro4::getLatch;
+
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        Thread t3 = new Thread(r3);
+        Thread t4 = new Thread(r4);
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        latch.await(1, TimeUnit.MINUTES);
+        System.out.println("Start to travell in metro");
+    }
+
+
+}
